cabaa1a2743645cb38ce96a44ad656d0be20dfaf
venkatrs-ch197 <65022470+venkatrs-ch197@users.noreply.github.com>
2020-05-30 00:07:37 +0530

    Deflaker example for 1.5-SNAPSHOT

---
+++ src/main/java/com/deflaker/DeFlaker/App.java
@@ -0,0 +1,13 @@
+package com.deflaker.DeFlaker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
